ea4d7c896d3d7ea80910d4f4d20c49bf8c0c4ddf
Jacob Rozran <rozran00@gmail.com>
2019-11-04 20:51:38 -0500

    fixed an issue with path

--- ecobee.R
+++ ecobee.R
@@ -1,69 +1,78 @@
-print(paste0(date(), ": Script Started"))
+print(paste0(Sys.time(), ": Script Started"))
+library(lubridate)
 
-library(httr)
-library(jsonlite)
-
-print(paste0(date(), ": Libraries Loaded. Refreshing Ecobee Creds"))
-
-creds <- read.csv("/home/ec2-user/ecobee/ecobee.config")
-
-refresh <- paste0("https://api.ecobee.com/token?grant_type=refresh_token&code=",
-                  creds$refresh_token[1], "&client_id=", creds$client_id[1])
-
-ref <- POST(refresh)
-
-at <- gsub("(^.*access_token\": \")(\\w+)(\".*$)", "\\2", as.character(ref))
-rt <- gsub("(^.*refresh_token\": \")(\\w+)(\".*$)", "\\2", as.character(ref))
-
-creds <- data.frame(access_token = at,
-                    refresh_token = rt,
-                    client_id = creds$client_id[1])
-
-write.csv(creds, "ecobee.config", row.names = FALSE)
-
-print(paste0(date(), ": Refreshed Ecobee Creds. Getting temps."))
-
-therm <- paste0("curl -s -H 'Content-Type: text/json' -H 'Authorization: Bearer ",
-                creds$access_token[1] , "' 'https://api.ecobee.com/1/thermostat?",
-                "format=json&body=\\{\"selection\":\\{\"selectionType\":\"regi",
-                "stered\",\"selectionMatch\":\"\",\"includeSensors\":true\\}",
-                "\\}' > response.json")
-
-system(therm)
-
-response <- read_json("response.json")
-
-print(paste0(date(), ": Got Temps. Formatting Data."))
-
-info <- data.frame()
-for(sensor in 1:3){
-    name <- response$thermostatList[[1]]$remoteSensors[[sensor]]$name
-    temp <- as.numeric(response$thermostatList[[1]]$remoteSensors[[sensor]]$capability[[1]]$value) / 10
+if(hour(Sys.time()) >= 22 | hour(Sys.time()) < 11){
+    library(httr)
+    library(jsonlite)
     
-    tmp <- data.frame(name = name,
-                      temp = temp)
-    tmp$time_utc <- response$thermostatList[[1]]$utcTime
-    tmp$time_local <- response$thermostatList[[1]]$thermostatTime
+    print(paste0(Sys.time(), ": Libraries Loaded. Refreshing Ecobee Creds"))
     
-    info <- rbind(info, tmp)
+    creds <- read.csv("/home/ec2-user/ecobee/ecobee.config")
+    
+    refresh <- paste0("https://api.ecobee.com/token?grant_type=refresh_token&code=",
+                      creds$refresh_token[1], "&client_id=", creds$client_id[1])
+    
+    ref <- POST(refresh)
+    
+    at <- gsub("(^.*access_token\": \")(\\w+)(\".*$)", "\\2", as.character(ref))
+    rt <- gsub("(^.*refresh_token\": \")(\\w+)(\".*$)", "\\2", as.character(ref))
+    
+    creds <- data.frame(access_token = at,
+                        refresh_token = rt,
+                        client_id = creds$client_id[1])
+    
+    write.csv(creds, "/home/ec2-user/ecobee/ecobee.config", row.names = FALSE)
+    
+    print(paste0(Sys.time(), ": Refreshed Ecobee Creds. Getting temps."))
+    
+    therm <- paste0("curl -s -H 'Content-Type: text/json' -H 'Authorization: Bearer ",
+                    creds$access_token[1] , "' 'https://api.ecobee.com/1/thermostat?",
+                    "format=json&body=\\{\"selection\":\\{\"selectionType\":\"regi",
+                    "stered\",\"selectionMatch\":\"\",\"includeSensors\":true\\}",
+                    "\\}' > response.json")
+    
+    system(therm)
+    
+    response <- read_json("response.json")
+    
+    print(paste0(Sys.time(), ": Got Temps. Formatting Data."))
+    
+    info <- data.frame()
+    for(sensor in 1:3){
+        name <- response$thermostatList[[1]]$remoteSensors[[sensor]]$name
+        temp <- as.numeric(response$thermostatList[[1]]$remoteSensors[[sensor]]$capability[[1]]$value) / 10
+        
+        tmp <- data.frame(name = name,
+                          temp = temp)
+        tmp$time_utc <- response$thermostatList[[1]]$utcTime
+        tmp$time_local <- response$thermostatList[[1]]$thermostatTime
+        
+        info <- rbind(info, tmp)
+    }
+    
+    info$name <- tolower(gsub("'s Room", "", info$name))
+    info <- info[info$name != "my ecobee", ]
+    
+    info$action <- ifelse(info$temp < 74, "on", "off")
+    
+    print(paste0(Sys.time(), ": Formatted Data. Telling VeSync what to do."))
+    
+    print(paste0(Sys.time(), ": here is the current status of each room and the state it should be in:"))
+    print(info)
+    
+    for(python in 1:dim(info)[1]){
+        py_cmd <- paste0("python3 vesync.py ", info$name[python], " ", 
+                         info$action[python], 
+                         " >> /home/ec2-user/ecobee/vesync.log 2>&1")
+        
+        system(py_cmd)
+    }
+    
+    print(paste0(Sys.time(), ": Updated the rooms as needed. Script Complete"))
+    
+} else {
+    print(paste0(Sys.time(), ": Daytime - not running. Script Complete"))
 }
 
-info$name <- tolower(gsub("'s Room", "", info$name))
-info <- info[info$name != "my ecobee", ]
 
-info$action <- ifelse(info$temp < 74, "on", "off")
 
-print(paste0(date(), ": Formatted Data. Telling VeSync what to do."))
-
-print(paste0(date(), ": here is the current status of each room and the state it should be in:"))
-print(info)
-
-for(python in 1:dim(info)[1]){
-    py_cmd <- paste0("python3 vesync.py ", info$name[python], " ", 
-                     info$action[python], 
-                     " >> /home/ec2-user/ecobee/vesync.log 2>&1")
-    
-    system(py_cmd)
-}
-
-print(paste0(date(), ": Updated the rooms as needed. Script Complete"))
